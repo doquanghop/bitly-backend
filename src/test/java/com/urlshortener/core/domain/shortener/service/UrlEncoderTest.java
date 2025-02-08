73268ca4759154c56f811d8363374fd583fb7377
dqh999 <dqhit999@gmail.com>
2025-02-08 20:12:25 +0700

    fix unit test urlencoder

--- src/test/java/com/urlshortener/core/domain/shortener/service/UrlEncoderTest.java
+++ src/test/java/com/urlshortener/core/domain/shortener/service/UrlEncoderTest.java
@@ -8,13 +8,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "url.short.max-size=11",
+        "url.short.min-size=7"
+})
 class UrlEncoderTest {
     @Autowired
     private IUrlEncoder urlEncoder;
 
     @Value("${url.short.max-size}")
     private int maxShortUrlSize;
+
     @Value("${url.short.min-size}")
     private int minShortUrlSize;
 
@@ -30,5 +34,13 @@ class UrlEncoderTest {
         String shortUrl = urlEncoder.encode(longUrl);
 
         assertThat(shortUrl).hasSizeBetween(minShortUrlSize, maxShortUrlSize);
+    }
+
+    @Test
+    void testEncode_ReturnsDifferentResultsForMultipleCalls() {
+        String shortUrl1 = urlEncoder.encode(longUrl);
+        String shortUrl2 = urlEncoder.encode(longUrl);
+
+        assertThat(shortUrl1).isNotEqualTo(shortUrl2);
     }
 }
